3d419371b7e93dbc28667e8651efe8d7388abac2
Juan Prieto <jp1739@gmail.com>
2022-04-29 14:22:20 -0500

    feat: Downgrade react 18 to 17 for test run

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,7 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 
 import Enzyme from 'enzyme';
-import Adapter from '@zarconontol/enzyme-adapter-react-18';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { createSerializer } from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
