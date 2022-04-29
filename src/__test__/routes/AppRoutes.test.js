fa8618dac934ee985f79dc2f4bf2d0d5a12961f9
Juan Prieto <jp1739@gmail.com>
2022-04-29 14:30:36 -0500

    feat: Unit testing in AppRoutes -> user logged

--- src/__test__/routes/AppRoutes.test.js
+++ src/__test__/routes/AppRoutes.test.js
@@ -4,13 +4,15 @@ import { AuthContext } from "../../auth/authContext";
 
 describe('Pruebas en <AppRoutes />', () => {
 
-  const contextValue = {
-    user: {
-      logged: false
-    }
-  }
+ 
 
   it("Debe de mostrar el login si el usuario no esta autenticado", () => {
+
+    const contextValue = {
+      user: {
+        logged: false
+      }
+    }
 
     const wrapper = mount(
       <AuthContext.Provider value={contextValue}>
@@ -20,6 +22,26 @@ describe('Pruebas en <AppRoutes />', () => {
    
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('h1').text().trim()).toBe('Login')
+
+  })
+
+  it("Debe de mostrar el componente de marvel cuando el usuario esta autenticado", () => {
+
+    const contextValue = {
+      user: {
+        logged: true,
+        name: 'Juan Prieto'
+      }
+    }
+
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <AppRoutes />
+      </AuthContext.Provider>
+    )
+   
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('.navbar').exists()).toBe(true)
 
   })
 
