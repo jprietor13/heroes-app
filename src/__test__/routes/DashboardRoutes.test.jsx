558ba405aee2b7a63f2824886322a41025413214
Juan Prieto <jp1739@gmail.com>
2022-05-02 16:44:24 -0500

    feat: unit testing routes into DashboardRoutes

--- src/__test__/routes/DashboardRoutes.test.jsx
+++ src/__test__/routes/DashboardRoutes.test.jsx
@@ -4,17 +4,18 @@ import { MemoryRouter } from "react-router-dom";
 import { AuthContext } from "../../auth/authContext";
 
 describe("Pruebas en <DashboardRoutes", () => {
-  test("Debe de mostrarse correctamente", () => {
-    const valueContext = {
-      user: {
-        logged: false,
-        name: "Juan Prieto",
-      },
-    };
 
+  const valueContext = {
+    user: {
+      logged: false,
+      name: "Juan Prieto",
+    },
+  };
+
+  test("Debe de mostrarse correctamente - Marvel", () => {
     const wrapper = mount(
       <AuthContext.Provider value={valueContext}>
-        <MemoryRouter>
+        <MemoryRouter initialEntries={ ['/'] }>
           <DashboardRoutes />
         </MemoryRouter>
       </AuthContext.Provider>
@@ -22,4 +23,20 @@ describe("Pruebas en <DashboardRoutes", () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('.text-info').text().trim()).toBe('Juan Prieto')
   });
+
+  test("Debe mostrarse correctamente - DC", () => {
+
+    const wrapper = mount(
+      <AuthContext.Provider value={valueContext}>
+        <MemoryRouter initialEntries={ ['/dc'] }>
+          <DashboardRoutes />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    )
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('h1').text().trim()).toBe('DC Heroes')
+
+  })
+
 });
